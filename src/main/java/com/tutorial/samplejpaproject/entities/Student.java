d32070f44c9201e00a05cd0ade3df18f2be210be
Charles Okafor <ckokafor@hotmail.com>
2021-03-10 13:17:45 +0000

    - code update (lombok related).

--- src/main/java/com/tutorial/samplejpaproject/entities/Student.java
+++ src/main/java/com/tutorial/samplejpaproject/entities/Student.java
@@ -1,13 +1,10 @@
 package com.tutorial.samplejpaproject.entities;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 
-@ToString
+@Data
 @NoArgsConstructor
 @Entity
 @Table (name = "student")
@@ -15,38 +12,24 @@ public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
-    @Setter
     @Column(name = "id", nullable = false, updatable = false)
     private Long id;
 
-    @Getter
-    @Setter
     @Column(name = "first_name", nullable = false, columnDefinition = "TEXT")
     private String firstname;
 
-    @Getter
-    @Setter
     @Column(name = "last_name", nullable = false, columnDefinition = "TEXT")
     private String lastname;
 
-    @Getter
-    @Setter
     @Column(name = "email", nullable = false, columnDefinition = "TEXT")
     private String email;
 
-    @Getter
-    @Setter
     @Column(name = "age", nullable = false)
     private int age;
-    
-    @Getter
-    @Setter
+
     @Column(name = "date_created", nullable = false, columnDefinition = "DATE")
     private String dateCreated;
-    
-    @Getter
-    @Setter
+
     @Column(name = "date_updated", nullable = false, columnDefinition = "DATE")
     private String dateUpdated;
 
